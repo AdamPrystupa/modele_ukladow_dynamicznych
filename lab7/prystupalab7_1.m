afe41bd0df048886581a6e20e6018663fd4d089a
AdamPrystupa <PrystupaAdam@gmail.com>
2023-11-14 09:53:29 +0100

    poprawka przygotowania

--- lab7/prystupalab7_1.m
+++ lab7/prystupalab7_1.m
@@ -6,7 +6,7 @@ clear all;
 close all;
 
 %stałe
-t0=10;
+t0=5;
 g=9.8;
 ts=100;
 
@@ -22,15 +22,15 @@ Aw2=0.6;
 H2=4;
 
 %wspolczynniki
-a1=sqrt((2*g*Aw1^2*(H1-H2))/(H1-H2)^2);
-a2=sqrt((2*g*Aw2^2)/H2);
+% a1=sqrt(2*g*Aw1^2/H1^2);
+% a2=sqrt(2*g*Aw2^2/H2^2);
 
 %maksymalne przepływy
-fwmax1=a1*(H1-H2);
-fwmax2=a2*H2-fwmax1;
+fwmax1=Aw1*sqrt(2*g*(H1-H2));
+fwmax2=Aw2*sqrt(2*g*H2)-Aw1*sqrt(2*g*H1-H2);
 
 dfw1=0.1*fwmax1;
-fw2=0.1*fwmax2;
+fw2=0.1*fwmax1;
 dfw2=0;  
 
 tabFw1=[0, 0.5*fwmax1, 0.9*fwmax1];
@@ -40,9 +40,11 @@ fw1=tabFw1(i);
 
     
 %punkty równowagi
-H02=(fw1+fw2)/a2;
-H01=(fw1)/a1+H02;
-[t] = sim('prystupalab7_si.slx',ts);
+H20=(fw2+fw1)^2/(Aw2^2*2*g);
+H10=(fw1^2)/(Aw1^2*2*g)+H20;
+
+
+[t] = sim('prystupalab7_1_si.slx',ts);
 
     figure(1);grid on, hold on; 
     set(gca, 'FontSize', 15, 'FontWeight','bold');
@@ -69,27 +71,32 @@ tabFw2=[0, 0.5*fwmax2, 0.9*fwmax2];
 for i=1:3;
 fw2=tabFw2(i);
 
-
-    
 %punkty równowagi
-H02=(fw1+fw2)/a2;
-H01=(fw1)/a1+H02;
-[t] = sim('prystupalab7_si.slx',ts);
+H20=(fw2+fw1)^2/(Aw2^2*2*g);
+H10=(fw1^2)/(Aw1^2*2*g)+H20;
 
-    figure(3);grid on, hold on; 
+
+[t] = sim('prystupalab7_1_si.slx',ts);
+
+    figure(1);grid on, hold on; 
     set(gca, 'FontSize', 15, 'FontWeight','bold');
     plot(t.tout, t.up, 'LineWidth',3);
     xlabel('t [s]',FontSize=15);
     ylabel('h(s)',FontSize=15);
     title('Zbiornik górny');
 
-    figure(4);grid on, hold on; 
+    figure(2);grid on, hold on; 
     set(gca, 'FontSize', 15, 'FontWeight','bold');
     plot(t.tout, t.down, 'LineWidth',3);
     xlabel('t [s]',FontSize=15);
     ylabel('h(s)',FontSize=15);
     title('Zbiornik dolny');
 end
+
+
+
+
+
 
 
 
